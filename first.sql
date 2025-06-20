8c7c13288e0112f13e4fbae91c8b6039806fae11
Mayhaul <mehulv2k6@gmail.com>
2025-06-20 19:14:12 +0530

    added notes and made changes in sql

--- first.sql
+++ first.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE college;
+CREATE DATABASE IF NOT EXISTS college;
 use college;
- CREATE TABLE student(
+ CREATE TABLE IF NOT EXISTS student(
  rollno INT PRIMARY KEY,
  name VARCHAR(50),
  marks INT NOT NULL,
@@ -8,7 +8,7 @@ use college;
  city VARCHAR(20)
  );
  
- INSERT INTO student
+ INSERT INTO  student
  (rollno,name,marks,grade,city)
  VALUES
  (101,"anil",78,"C","Pune"),
@@ -18,4 +18,33 @@ use college;
  (105,"emanuel",12,"E","Delhi"),
  (106,"farah",82,"B","Delhi");
  
- SELECT DISTINCT city FROM student;
+SELECT city,AVG(marks) AS avg_marks
+FROM student
+GROUP BY city
+HAVING avg_marks >70 
+ORDER BY AVG(marks) ASC;
+
+CREATE DATABASE IF NOT EXISTS sales;
+USE sales;
+CREATE TABLE IF NOT EXISTS customer_data (
+    customer_id INT PRIMARY KEY,
+    customer VARCHAR(50),
+    mode VARCHAR(20),
+    city VARCHAR(30)
+);
+INSERT INTO customer_data (customer_id, customer, mode, city)
+VALUES
+(101, 'Olivia Barrett', 'Netbanking', 'Portland'),
+(102, 'Ethan Sinclair', 'Credit Card', 'Miami'),
+(103, 'Maya Hernandez', 'Credit Card', 'Seattle'),
+(104, 'Liam Donovan', 'Netbanking', 'Denver'),
+(105, 'Sophia Nguyen', 'Credit Card', 'New Orleans'),
+(106, 'Caleb Foster', 'Debit Card', 'Minneapolis'),
+(107, 'Ava Patel', 'Debit Card', 'Phoenix'),
+(108, 'Lucas Carter', 'Netbanking', 'Boston'),
+(109, 'Isabella Martinez', 'Netbanking', 'Nashville'),
+(110, 'Jackson Brooks', 'Credit Card', 'Boston');
+
+SELECT mode,COUNT(customer)
+FROM customer_data
+GROUP BY mode;
